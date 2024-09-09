58888bb1930a97150a33385bedde7828a3ba7b58
B-Acharekar <153992410+B-Acharekar@users.noreply.github.com>
2024-09-09 18:24:14 +0530

    Re structural commit

---
+++ src/main/java/com/crowdconnect/CrowdconnectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.crowdconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowdconnectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowdconnectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crowdconnect/CrowdconnectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crowdconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdconnectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
